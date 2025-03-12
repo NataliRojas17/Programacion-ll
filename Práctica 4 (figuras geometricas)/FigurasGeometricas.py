4b3c3565ef0fe9c2a331d9356e1dc5016ea7f854
NataliRojas17 <nrojasa@fcpn.edu.bo>
2025-03-12 19:48:10 -0400

    Update FigurasGeometricas.py

--- Práctica 4 (figuras geometricas)/FigurasGeometricas.py
+++ Práctica 4 (figuras geometricas)/FigurasGeometricas.py
@@ -1,36 +1,31 @@
 import math
 
 class FigurasGeometricas:
-
     def area(self, *args):
-        if len(args) == 1:  # Círculo
+        if len(args) == 1:
             radio = args[0]
-            return math.pi * radio * radio
-        
-        elif len(args) == 2:  
-            base, altura = args
-            return base * altura  # Rectángulo
-        
+            return math.pi * radio * radio   # circulo
+        elif len(args) == 2:
+            if isinstance(args[0], float) and isinstance(args[1], float):
+                b, h = args
+                return (b * h) / 2     # triangulo rectángulo 
+            else:
+                if isinstance(args[0], int) and isinstance(args[1], float):
+                    lado, apotema = args
+                    return ((5 * lado) * apotema) / 2   #pentagono
+                else:
+                    if isinstance(args[0], int) and isinstance(args[1], int):
+                        b, h = args
+                        return b * h    #rectángulo 
         elif len(args) == 3:
-            if isinstance(args[1], float):  # Triángulo rectángulo
-                b, h = args[0], args[1]
-                return (b * h) / 2
-            else:                           # Trapecio
-                lado1, lado2, alto = args
-                return ((lado1 + lado2) * alto) / 2
-        
-        elif len(args) == 2 and isinstance(args[0], float):  # Pentágono
-            lado, apotema = args
-            return (5 * lado * apotema) / 2
-        
+            l1, l2, h = args
+            return ((l1 + l2) * h) / 2  #trapecio
         else:
             raise ValueError("Parámetro no válido")
-        
-
 f = FigurasGeometricas()
 
-print("Círculo:", f.area(3))
-print("Rectángulo:", f.area(4, 6))
-print("Triángulo Rectángulo:", f.area(5, 9.0))
-print("Trapecio:", f.area(8, 3, 5))
-print("Pentágono:", f.area(9, 4.5))
+print("Círculo: ", f.area(3))
+print("Rectángulo: ", f.area(4, 6)) 
+print("Triángulo Rectángulo: ", f.area(5.0, 9.0))  
+print("Trapecio: ", f.area(8, 3, 5))  
+print("Pentágono: ", f.area(9, 4.5))
