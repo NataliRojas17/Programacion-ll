e5392d343811e3188fc2794e0a90e0d258d85afa
NataliRojas17 <nrojasa@fcpn.edu.bo>
2025-03-31 18:54:55 -0400

    Update Algebra Vectorial.java

--- Práctica 6 (Polimorfismo)/Algebra Vectorial.java
+++ Práctica 6 (Polimorfismo)/Algebra Vectorial.java
@@ -7,18 +7,6 @@ public class AlgebraVectorial {
         this.z = z;
     }
 
-    public AlgebraVectorial(double x, double y) {
-        this(x, y, 0);
-    }
-
-    public AlgebraVectorial sumar(AlgebraVectorial o) {
-        return new AlgebraVectorial(this.x + o.x, this.y + o.y, this.z + o.z);
-    }
-
-    public AlgebraVectorial restar(AlgebraVectorial o) {
-        return new AlgebraVectorial(this.x - o.x, this.y - o.y, this.z - o.z);
-    }
-
     public double productoPunto(AlgebraVectorial o) {
         return this.x * o.x + this.y * o.y + this.z * o.z;
     }
@@ -35,36 +23,24 @@ public class AlgebraVectorial {
         return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
     }
 
-    public boolean esPerpendicular(AlgebraVectorial o) {
-        return this.productoPunto(o) == 0;
-    }
-
-    public boolean esParalelo(AlgebraVectorial o) {
-        return this.productoCruz(o).magnitud() == 0;
-    }
-
     public AlgebraVectorial proyeccion(AlgebraVectorial o) {
-        double escala = this.productoPunto(o) / (o.magnitud() * o.magnitud());
+        double magO = o.magnitud();
+        if (magO == 0 || this.productoPunto(o) == 0) return null;  // Condición corregida
+        double escala = this.productoPunto(o) / (magO * magO);
         return new AlgebraVectorial(escala * o.x, escala * o.y, escala * o.z);
-    }
-
-    public double componente(AlgebraVectorial o) {
-        return this.productoPunto(o) / o.magnitud();
     }
 
     @Override
     public String toString() {
-        return x + ", " + y + ", " + z ;
+        return "(" + x + ", " + y + ", " + z + ")";
     }
 
     public static void main(String[] args) {
-        AlgebraVectorial a = new AlgebraVectorial(6, -3, 9);
-        AlgebraVectorial b = new AlgebraVectorial(4, 2, -6);
+        AlgebraVectorial a = new AlgebraVectorial(1, 2, 3);
+        AlgebraVectorial b = new AlgebraVectorial(-2, 1, 0);
 
-        System.out.println("¿Son perpendiculares? " + a.esPerpendicular(b));
-        System.out.println("¿Son paralelos? " + a.esParalelo(b));
-        System.out.println("Proyeccion de a sobre b: " + a.proyeccion(b));
-        System.out.println("Componente de a en b: " + a.componente(b));
+        System.out.println("Producto escalar = " + a.productoPunto(b));
+        System.out.println("Producto Vectorial = " + a.productoCruz(b));
+        System.out.println("Proyección de a sobre b = " + a.proyeccion(b));
     }
 }
-
